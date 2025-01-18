fa2f1aeccdfab4db4dbeaa92a00bdfbd8ed33b6c
SoufianeMoussaoui <dgiugfha098@gmail.com>
2025-01-18 19:03:30 +0100

    Add files via upload

---
+++ includes/leftbar.php
@@ -0,0 +1,63 @@
+<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
+<div class="left-sidebar bg-black-300 box-shadow ">
+                        <div class="sidebar-content">
+                            <div class="user-info closed">
+                                <img src="https://example.com/admin-icon.png" class="img-circle profile-img">
+
+                                <h6 class="title">Admin</h6>
+                                <small class="info">Administrateur</small>
+                            </div>
+                            <!-- /.user-info -->
+
+                            <div class="sidebar-nav">
+                                <ul class="side-nav color-gray">
+                                    <li class="nav-header">
+                                        <span class="">Main Category</span>
+                                    </li>
+                                    <li>
+                                        <a href="dashboard.php"><i class="fa fa-dashboard"></i> <span>Dashboard</span> </a>
+                                     
+                                    </li>
+
+                                    <li class="nav-header">
+                                        <span class="">Appearance</span>
+                                    </li>
+                                    <li class="has-children">
+                                        <a href="#"><i class="fa fa-file-text"></i> <span>Matière</span> <i class="fa fa-angle-right arrow"></i></a>
+                                        <ul class="child-nav">
+                                            <li><a href="create-class.php"><i class="fa fa-bars"></i> <span>Ajouter Matière</span></a></li>
+                                            <li><a href="manage-classes.php"><i class="fa fa fa-server"></i> <span>Gestions des Matières</span></a></li>
+                                           
+                                        </ul>
+                                    </li>
+  
+                                    </li>
+   <li class="has-children">
+                                        <a href="#"><i class="fa fa-users"></i> <span>Étudiants</span> <i class="fa fa-angle-right arrow"></i></a>
+                                        <ul class="child-nav">
+                                            <li><a href="add-students.php"><i class="fa fa-bars"></i> <span>Ajouter Étudiant</span></a></li>
+                                            <li><a href="manage-students.php"><i class="fa fa fa-server"></i> <span>Gestions des Étudiants</span></a></li>
+                                           
+                                        </ul>
+                                    </li>
+
+
+
+<li class="has-children">
+                                        <a href="#">  <i class="fa fa-chalkboard-teacher" aria-hidden="true"></i><span>Enseignant</span> <i class="fa fa-angle-right arrow"></i></a>
+                                        <ul class="child-nav">
+                                            <li><a href="add-enseignant.php"><i class="fa fa-bars"></i> <span>Ajouter Enseignant</span></a></li>
+                                            <li><a href="gestion-enseignant.php"><i class="fa fa fa-server"></i> <span>Gestions des Enseignants</span></a></li>
+                                           
+                                        </ul>        </li>
+
+
+
+                                        <li><a href="change-password.php"><i class="fa fa-key" aria-hidden="true"></i><span>Changer votre mot de passe</span></a></li>
+                                           
+                            
+                            </div>
+                            <!-- /.sidebar-nav -->
+                        </div>
+                        <!-- /.sidebar-content -->
+                    </div>
